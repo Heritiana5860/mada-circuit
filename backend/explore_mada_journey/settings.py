00b5fadba7e7089fe42b490dbc190812166c3036
Heritiana5860 <heritiana5860@gmail.com>
2025-09-20 20:36:09 +0300

    settings

--- backend/explore_mada_journey/settings.py
+++ backend/explore_mada_journey/settings.py
@@ -30,10 +30,10 @@ try:
     original_csrf_middleware = django.middleware.csrf.CsrfViewMiddleware
     
     class DummyCSRFMiddleware:
-        def _init_(self, get_response):
+        def __init__(self, get_response):
             self.get_response = get_response
         
-        def _call_(self, request):
+        def __call__(self, request):
             setattr(request, '_dont_enforce_csrf_checks', True)
             return self.get_response(request)
         
